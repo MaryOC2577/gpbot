6d81c39df43eb9ba894045ad51706697eed5fc7b
Maryline <marybot@free.fr>
2021-06-17 21:27:54 +0200

    test json

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/json')
+def get_bot_response():
+    return render_template('get_bot_response.json')
