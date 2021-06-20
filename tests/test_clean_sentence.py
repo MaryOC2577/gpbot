986e2a4ea439216d120a29d57841d2208d69274c
Maryline <marybot@free.fr>
2021-06-20 15:28:32 +0200

    tests parser

--- tests/test_clean_sentence.py
+++ tests/test_clean_sentence.py
@@ -1,18 +1,29 @@
 import pytest
 from app.models.clean_sentence import CleanSentence
 
-# tester si la phrase est une liste 
-def split_list_sentence(self):
-    sentence = "une phrase longue pour tester"
-    result = CleanSentence().split_sentence(sentence)
-    return result
+class TestCleanSentence:
 
-def test_split_list_sentence():
-    assert isinstance(split_list_sentence(), list) == True
+    # tester si la phrase est une liste 
+    def split_list_sentence(self):
+        sentence = "absolument rendre à"
+        return CleanSentence().split_sentence(sentence)
+     
+    def test_split_list_sentence(self):
+        assert isinstance(self.split_list_sentence(), list) == True
 
-# tester si la liste contient des string
+    # tester si la liste contient des string
+    def test_string_in_list(self):
+        for element in self.split_list_sentence():
+            assert isinstance(element, str) == True
 
-# tester si la liste contient des stops words
-#def test_cleanup_sentence_nostopwords(sentence):
+    # tester si les mots sont en minuscules
 
-# tester si la liste contient un lieu
+    # tester si les mots de la liste contiennent des accents, caractères spéciaux...
+
+    # tester si la liste ne contient pas des stops words
+    def test_cleanup_sentence_nostopwords(self):
+        for element in self.split_list_sentence():
+            for words in CleanSentence().stop_words:
+                assert element != words
+
+
