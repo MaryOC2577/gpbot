4cec6b21408369887c6e5ac429994de0195be00a
Maryline <marybot@free.fr>
2021-07-01 21:16:15 +0200

    add gps coordinates

---
+++ heregps.py
@@ -0,0 +1,11 @@
+import requests
+from pprint import pprint
+
+url = "https://geocode.search.hereapi.com/v1/geocode"
+params = {
+    "apiKey": "nA_2GT2YF3clqlab3lCR8BNVlyVeSdcnmZ2Co_6d9VE",
+    "q": "stade de france",
+}
+
+response = requests.get(url, params=params)
+pprint(response.json())
