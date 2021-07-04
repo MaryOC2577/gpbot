d3dceae97b18b1e92f957446f095713b6288d987
Maryline <marybot@free.fr>
2021-07-04 15:48:26 +0200

    update parser

--- app/models/clean_sentence.py
+++ app/models/clean_sentence.py
@@ -1,9 +1,14 @@
 """Clean a sentence."""
 
-from app.models.stop_words import list_words
+import json
 
 
 class CleanSentence:
+    def load_stop_words(self):
+        with open("app/data/stopwords.json", "r") as json_data:
+            stop_words = json.load(json_data)
+        return stop_words
+
     def clear_sentence(self, sentence):
         """Clear a sentence into a list."""
         result = str(sentence).lower().split()
@@ -11,10 +16,11 @@ class CleanSentence:
 
     def delete_stop_words(self, list_sentence):
         """Delete stop_words in a list."""
+        list_words = self.load_stop_words()
         for word in list_sentence:
             if word in list_words:
                 del list_sentence[list_sentence.index(word)]
         return list_sentence
 
     def clean_sentence(self, sentence):
-        self.delete_stop_words(self.clear_sentence(sentence))
+        return self.delete_stop_words(self.clear_sentence(sentence))
--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,7 @@ from flask.helpers import make_response
 from flask.json import jsonify
 from app import app
 from flask import render_template
-
-# from app.models.clean_sentence import CleanSentence
+from app.models.clean_sentence import CleanSentence
 
 
 @app.route("/")
@@ -14,15 +13,12 @@ def index():
     return render_template("index.html", title="Home")
 
 
-@app.route("/json")
-def get_bot_response():
-    return render_template("get_bot_response.json")
-
-
 @app.route("/find_place", methods=["POST"])
 def find_place():
     res = request.get_json()
-    print("sentence : ", res["text"])
-    # result = CleanSentence.clean_sentence(res)
-    # print("cleaned sentence : ", result)
+    clear_sentence = res["text"]
+    print("sentence : ", clear_sentence)
+    result = CleanSentence()
+    result.clean_sentence(clear_sentence)
+    print("cleaned sentence : ", result)
     return jsonify(cleaned_text=res["text"])
