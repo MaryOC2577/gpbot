31bf0f231b064d093846d88a8d040c55d38c07aa
Maryline <marybot@free.fr>
2021-07-19 18:27:13 +0200

    update routes

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,17 @@ import json
 from flask import request
 from flask.helpers import make_response
 from flask.json import jsonify
+
 from flask_cors.decorator import cross_origin
 from app import app
 from flask import render_template
 from app.models.clean_sentence import CleanSentence
 from app.models.gps_coordinates import HereGps
-from flask_cors import cross_origin
+
+from flask_cors import CORS
+
+CORS(app)
+cors = CORS(app, resources={r"/*": {"origins": "localhost"}})
 
 
 @app.route("/")
@@ -16,10 +21,11 @@ def index():
     return render_template("index.html", title="Home")
 
 
-@app.route("/find_place", methods=["POST"])
+@app.route("/find_place", methods=["GET", "POST"])
 @cross_origin()
 def find_place():
     res = request.get_json()
+    print(res)
     clear_sentence = res["text"]
     print("sentence : ", clear_sentence)
     result = CleanSentence()
