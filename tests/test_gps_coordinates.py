3c4b70f022b60ae58cc6f9558a1a0dba80129053
Mikael Briolet <mbriolet.ma@gmail.com>
2021-08-06 18:46:47 +0200

    Update test_gps_coordinates.py

--- tests/test_gps_coordinates.py
+++ tests/test_gps_coordinates.py
@@ -1,7 +1,7 @@
 import requests
 
 
-class GpsCoords:
+class GpsCoords:  # ne pas utiliser ce code ;) le but est de tester la classe de ton code source !
     def execute(self):
         return requests.get("https://geocode.search.hereapi.com/v1/geocode").json()
 
@@ -17,4 +17,4 @@ def test_execute(monkeypatch):
 
     monkeypatch.setattr(requests, "get", fake_get)
     coords = GpsCoords()
-    assert coords.execute() == {"lat": 48.87695, "lng": 2.29362}
+    assert coords.execute() == {"lat": 48.87695, "lng": 2.29362}  # il me semble que tu traites aussi l'adresse donc ajoute là ici aussi
