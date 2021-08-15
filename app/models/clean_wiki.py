ac960b3ebdd79560bd1ac2dce87ef6f73e3289c5
Maryline <marybot@free.fr>
2021-08-15 16:07:23 +0200

    add requirements

--- app/models/clean_wiki.py
+++ app/models/clean_wiki.py
@@ -16,10 +16,28 @@ class WikiAPI:
                 "namespace": 0,
             },
         ).json()
-        raw_html = response["parse"]["text"]["*"]
-        document = html.document_fromstring(raw_html)
-        first_p = document.xpath("//p")[0]
-        intro_text = first_p.text_content()
+        # raw_html = response["parse"]["text"]["*"]
+        # document = html.document_fromstring(raw_html)
+        # first_p = document.xpath("//p")[0]
+
+        url = "".join(response[3])
+        print("url :", url)
+        return url
+
+    def get_wiki_text(self, location):
+
+        wiki_url = self.find_wiki_text(location)
+        response = requests.get(
+            wiki_url,
+            params={
+                "action": "opensearch",
+                "search": location,
+                "format": "json",
+            },
+        ).json()
+
+        intro_text = response["extract"]
+        print("get wiki :", intro_text)
         if intro_text.isspace():
             return "Désolé GrandPy ne sais pas lire dans les pensées et n'as pas trouvé d'information concernant ce lieu. Veuillez reformuler votre demande."
         else:
