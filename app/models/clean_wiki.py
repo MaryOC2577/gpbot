abd05370d34e232dfa3ecddef0570c16add1f01d
Maryline <marybot@free.fr>
2021-08-16 14:59:51 +0200

    refacoring clean wiki

--- app/models/clean_wiki.py
+++ app/models/clean_wiki.py
@@ -16,29 +16,25 @@ class WikiAPI:
                 "namespace": 0,
             },
         ).json()
-        # raw_html = response["parse"]["text"]["*"]
-        # document = html.document_fromstring(raw_html)
-        # first_p = document.xpath("//p")[0]
-
         url = "".join(response[3])
-        print("url :", url)
         return url
 
     def get_wiki_text(self, location):
 
         wiki_url = self.find_wiki_text(location)
+        json_url = "https://fr.wikipedia.org/api/rest_v1/page/summary/" + location
         response = requests.get(
-            wiki_url,
+            json_url,
             params={
                 "action": "opensearch",
-                "search": location,
                 "format": "json",
             },
         ).json()
 
         intro_text = response["extract"]
-        print("get wiki :", intro_text)
+
         if intro_text.isspace():
             return "Désolé GrandPy ne sais pas lire dans les pensées et n'as pas trouvé d'information concernant ce lieu. Veuillez reformuler votre demande."
         else:
-            return intro_text
+            wiki_info = intro_text + wiki_url
+            return wiki_info
