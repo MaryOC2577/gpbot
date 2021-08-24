7af2a6302d0e19222af449e44f1e38b1c7e48068
Maryline <marybot@free.fr>
2021-08-24 21:29:30 +0200

    update tests

--- tests/test_gps_coordinates.py
+++ tests/test_gps_coordinates.py
@@ -6,16 +6,16 @@ from app.models.gps_coordinates import HereAPI
 class FakeResponse:
     def json(self):
         return {
-            "lat": 48.87695,
-            "lng": 2.3414,
+            "coords": {"lat": 48.85717, "lng": 2.3414},
             "adress": "Paris, Île-de-France, France",
         }
 
 
-def test_execute(monkeypatch):
+def test_get_coords(monkeypatch):
     def fake_get(*args, **kwargs):
         return FakeResponse()
 
-    monkeypatch.setattr(requests, "get", fake_get)
     coords = HereAPI()
-    assert coords.find_coordinates("paris") == test_execute()
+    location = "paris"
+    monkeypatch.setattr(requests, "get", fake_get)
+    assert coords.find_coordinates(location)
